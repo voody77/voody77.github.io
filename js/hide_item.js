7b40ae27ba77ba57a5b66e02d8fbb25a2753c156
voody77 <voody77@me.com>
2019-12-13 11:01:24 +0300

    Add site files

---
+++ js/hide_item.js
@@ -0,0 +1,15 @@
+// function toggle_visibility(id) {
+//     var e = document.getElementById(id);
+//     if (e.style.display == 'block')
+//         (e.style.display = 'none');
+//     else
+//         e.style.display = 'block';
+// }
+
+
+$(document).ready(function(){
+    $(".button").click(function () {
+        $(".hide").slideToggle({duration: 300}).scrollTop();
+        $("html, body").animate({scrollTop: $(document).height()})
+    });
+});
