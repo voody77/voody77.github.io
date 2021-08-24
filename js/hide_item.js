e2b69767a8a25ca2f15869159e7795e7f14fc64f
voody77 <vadim@kruchinin.moscow>
2021-08-24 17:09:30 +0300

    add last employer

--- js/hide_item.js
+++ js/hide_item.js
@@ -1,19 +1,3 @@
-// function toggle_visibility(id) {
-//     var e = document.getElementById(id);
-//     if (e.style.display == 'block')
-//         (e.style.display = 'none');
-//     else
-//         e.style.display = 'block';
-// }
-
-
-// $(function(){
-//     $(".button").click(function () {
-//         $(".hide").slideToggle({duration: 300}).scrollTop();
-//         $("html, body").animate({scrollTop: $(document).height('#job')})
-//     });
-// });
-
 $(function(){
     $('.button').click(function(){
         $('.hide').slideToggle(300);
